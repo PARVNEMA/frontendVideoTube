f3106167ef79e7f2c43262ddf2ff108141861063
parv nema <115030244+PARVNEMA@users.noreply.github.com>
2024-10-18 13:20:22 +0530

    made some responsive ness

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,18 @@ import Sidebar from "./Components/Sidebar";
 function App() {
 	return (
 		<div className="min-h-screen flex flex-col">
-  <div className="w-full">
-    <Navbar />
-  </div>
-  <div className="flex flex-grow ">
-    <div className="md:w-1/4 h-full sticky top-20 border-r-2 border-purple-500  ">
-      <Sidebar />
-    </div>
-    <main className="flex-grow p-2">
-      <Outlet />
-    </main>
-  </div>
-</div>
-
-
+			<div className="w-full">
+				<Navbar />
+			</div>
+			<div className="flex flex-grow">
+				<div className="hidden md:block md:w-1/4 md:h-full md:sticky md:top-20 md:border-r-2 md:border-purple-500">
+					<Sidebar />
+				</div>
+				<main className="flex-grow p-2">
+					<Outlet />
+				</main>
+			</div>
+		</div>
 	);
 }
 
